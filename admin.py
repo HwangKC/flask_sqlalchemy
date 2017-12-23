80e74035c206191b287f957ed83c67a544af12f8
saturn <vmto@qq.com>
2017-12-23 17:23:34 +0800

    update

--- admin.py
+++ admin.py
@@ -1,13 +1,19 @@
 # coding=utf-8
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 from flask_admin import Admin, BaseView, expose
+from flask_login import LoginManager
+
 from models.model import db, User
 from config import config
 
 app = Flask(__name__)
 app.config.from_object(config)
+
 admin = Admin(app, name='后台管理', template_mode='bootstrap3')
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 class Main(BaseView):
@@ -20,7 +26,7 @@ admin.add_view(Main(name='方案', endpoint='main', category='系统管理'))
 admin.add_view(Main(name='登录', endpoint='login', category='用户管理'))
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     users = User.query.all()
     arr = []
@@ -34,7 +40,7 @@ def index():
             'enabled': user.enabled
         })
 
-    return jsonify(arr)
+    return render_template('admin/login.html')
 
 
 if __name__ == '__main__':
--- login.py
+++ login.py
@@ -1,10 +1,10 @@
 # coding=utf-8
-from flask import Flask, jsonify, Blueprint, request, render_template
+from flask import Flask, Blueprint, request, render_template
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
 app = Flask(__name__)
 
-# add-1
+# test-1
 app.secret_key = 'xx00'
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,7 +12,7 @@ login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
 
-# add-2
+# test-2
 class User(UserMixin):
     def is_authenticated(self):
         return True
@@ -33,11 +33,11 @@ def load_user(user_id):
     return user
 
 
-# url redirect
+# url 重定向
 auth = Blueprint('auth', __name__)
 
 
-# test method
+# 权限测试
 @app.route('/')
 @app.route('/test')
 @login_required
@@ -45,17 +45,18 @@ def test():
     return render_template('admin/main.html')
 
 
+# 登录
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         username = request.args.get('username')
         password = request.args.get('password')
-        print('GET请求',username, password)
+        print('GET请求', username, password)
         return render_template('admin/login.html')
     else:
         username = request.form.get('username')
         password = request.form.get('password')
-        print('POST请求',username, password)
+        print('POST请求', username, password)
         if username == 'admin' and password == '123456':
             user = User()
             login_user(user)
@@ -63,6 +64,7 @@ def login():
         return render_template('admin/login.html')
 
 
+# 退出
 @auth.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
